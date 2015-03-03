2372d853100fd998793d1e142df980c16adf3c7f
James Talmage <james.talmage@jrtechnical.com>
2015-03-02 22:26:48 -0500

    handle source-maps internally

--- src/index.js
+++ src/index.js
@@ -3,17 +3,34 @@ module.exports = transform;
 var recast = require('recast');
 var ngProvide = require('./ngProvide/index');
 var ngInject = require('./ngInject');
+var convert = require('convert-source-map');
 
-function transform(src, opts){
-  opts = opts || {};
-  if(opts.sourceFileName && !opts.sourceMapName){
-    opts.sourceMapName = opts.sourceFileName + '.map';
+function transform(src, suppliedOptions){
+  var options = {};
+  suppliedOptions = suppliedOptions || {};
+  if(suppliedOptions.sourceFileName) options.sourceFileName = suppliedOptions.sourceFileName;
+  if(suppliedOptions.sourceFileName && !suppliedOptions.sourceMapName){
+    options.sourceMapName = suppliedOptions.sourceFileName + '.map';
   }
-  opts.inputSourceMap = opts.inputSourceMap || null;
-  var doNgInject = opts.hasOwnProperty('ngInject') ? opts.ngInject : true;
-  var doNgProvide = opts.hasOwnProperty('ngProvide') ? opts.ngProvide : true;
-  var ast = recast.parse(src, opts);
+  if(suppliedOptions.inputSourceMap){
+    options.inputSourceMap = suppliedOptions.inputSourceMap;
+  } else if(suppliedOptions.readSourceMapComments !== false) {
+    options.inputSourceMap = convert.fromSource(src);
+  }
+  var doNgInject = suppliedOptions.hasOwnProperty('ngInject') ? suppliedOptions.ngInject : true;
+  var doNgProvide = suppliedOptions.hasOwnProperty('ngProvide') ? suppliedOptions.ngProvide : true;
+  var ast = recast.parse(src, options);
   if(doNgProvide) ngProvide(ast);
   if(doNgInject) ngInject(ast);
-  return recast.print(ast, opts);
+  var result = recast.print(ast, options);
+  var transformedCode = result.code;
+  var map = result.map;
+  if(map && suppliedOptions.appendSourceMapComment){
+    transformedCode = convert.removeComments(transformedCode);
+    transformedCode += '\n' + convert.fromObject(map).toComment() + '\n';
+  }
+  return {
+    code: transformedCode,
+    map: map
+  };
 }
--- test/index-test.js
+++ test/index-test.js
@@ -1,20 +1,23 @@
 describe('main', function() {
   var process;
-  var proxyquire = require('proxyquire');
-  var sinon = require('sinon');
+  var proxyquire    = require('proxyquire');
+  var sinon         = require('sinon');
+  var convert       = require('convert-source-map');
+  var input, pathToIndex;
 
   beforeEach(function (){
-    process = require('../src/index');
-  });
-
-  it('will not create sourcemap by default', function() {
-    var input = [
+    pathToIndex = '../src/index';
+    process = require(pathToIndex);
+    input = [
       'var e, f;',
       '// @ngInject',
       'var c, d;',
       '// @ngProvide',
       'var a = "a", b = "b";'
     ].join('\n');
+  });
+
+  it('will not create sourcemap by default', function() {
     var output = process(input);
     expect(output.code).to.equal([
       'var e, f;',
@@ -43,13 +46,6 @@ describe('main', function() {
   });
 
   it('ngInject can be turned off', function() {
-    var input = [
-      'var e, f;',
-      '// @ngInject',
-      'var c, d;',
-      '// @ngProvide',
-      'var a = "a", b = "b";'
-    ].join('\n');
     var output = process(input, {ngInject:false});
     expect(output.code).to.equal([
       'var e, f;',
@@ -73,13 +69,6 @@ describe('main', function() {
   });
 
   it('ngProvide can be turned off', function() {
-    var input = [
-      'var e, f;',
-      '// @ngInject',
-      'var c, d;',
-      '// @ngProvide',
-      'var a = "a", b = "b";'
-    ].join('\n');
     var output = process(input,{ngProvide:false});
     expect(output.code).to.equal([
       'var e, f;',
@@ -99,33 +88,12 @@ describe('main', function() {
   });
 
   it('ngProvide and ngInject can be turned off', function() {
-    var input = [
-      'var e, f;',
-      '// @ngInject',
-      'var c, d;',
-      '// @ngProvide',
-      'var a = "a", b = "b";'
-    ].join('\n');
     var output = process(input,{ngProvide:false,ngInject:false});
-    expect(output.code).to.equal([
-      'var e, f;',
-      '// @ngInject',
-      'var c, d;',
-      '// @ngProvide',
-      'var a = "a", b = "b";'
-    ].join('\n'));
-
+    expect(output.code).to.equal(input);
     expect(output.map).to.equal(undefined);
   });
 
   it('will create a sourcemap if sourceFileName is set', function() {
-    var input = [
-      'var e, f;',
-      '// @ngInject',
-      'var c, d;',
-      '// @ngProvide',
-      'var a = "a", b = "b";'
-    ].join('\n');
     var output = process(input,{sourceFileName:'src.js'});
     expect(output.code).to.equal([
       'var e, f;',
@@ -152,17 +120,43 @@ describe('main', function() {
     expect(!!output.map).to.equal(true);
   });
 
-  it('will pass through sourceFileName and inputSourceMap', function() {
+  it('will create a sourcemap if sourceFileName is set', function() {
+    var output = process(input,{sourceFileName:'src.js', appendSourceMapComment:true});
+    expect(convert.fromSource(output.code)).not.to.equal(null);
+  });
+
+  it('will read sourcemap from comments if inputSourceMap is not supplied', function(){
+    var fromSource = sinon.stub();
     var parse = sinon.spy();
-    var print = sinon.spy();
-    var insertVariableInjections = sinon.spy();
-    process = proxyquire('../src/',{
+    var print = sinon.stub();
+    process = proxyquire(pathToIndex,{
       'recast':{
         parse:parse,
         print:print
       },
-      './insertVariableInjections':insertVariableInjections
+      'convert-source-map': {
+        fromSource: fromSource
+      }
     });
+    print.returns({code:'blah'});
+    process(input);
+    expect(fromSource.called).to.equal(true);
+    fromSource.reset();
+    process(input,{readSourceMapComments:false}) ;
+    expect(fromSource.called).to.equal(false);
+  });
+
+  it('will pass through sourceFileName and inputSourceMap', function() {
+    var parse = sinon.spy();
+    var print = sinon.stub();
+    process = proxyquire('../src/',{
+      'recast':{
+        parse:parse,
+        print:print
+      }
+    });
+
+    print.returns({code:'blahblah',map:'map'});
 
     process('var a;',{sourceFileName:'input.src',inputSourceMap:{a:'a',b:'b'}});
 
