524a6dc57a247d79cbc3243625c59c100105968a
James Talmage <james.talmage@jrtechnical.com>
2015-03-04 03:11:31 -0500

    remove ngInject/buildInjectionCode. it had become so trivial as to no longer require it's own module.

--- src/ngInject/index.js
+++ src/ngInject/index.js
@@ -8,7 +8,6 @@ var s = require('../utils/builders');
 function createInjector (regexp, logger) {
 
   var needsInjection = require('./needsInjection').create(regexp, logger);
-  var buildInjectionCode = require('./buildInjectionCode');
   var collectVariableIds = require('./collectVariableIds');
 
   return addVariableInjections;
@@ -19,9 +18,17 @@ function createInjector (regexp, logger) {
         var node = path.node;
         if (needsInjection(node)) {
           var obj = collectVariableIds(node);
+
+          // variable declaration
           var decl = s.variableDeclaration(obj.ids);
           decl.comments = node.comments;
-          path.replace(decl, buildInjectionCode(obj));
+
+          // beforeEach call that injects required variables and assigns them.
+          var beforeEach = s.beforeEachStmt([
+            s.injectCall(obj.inject, obj.assign)
+          ]);
+
+          path.replace(decl, beforeEach);
         }
         return false;
       }
