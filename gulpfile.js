95e1c687e03665994028989302114ee10acad503
James Talmage <james@talmage.io>
2015-03-18 14:35:07 -0400

    update npm ignore

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var karma = require('karma').server;
 var fs = require('fs');
 var del = require('del');
 var lock = require('gulp-lock');
-var _ = require('lodash');
 
 gulp.task('lint', function() {
   return gulp.src(['src/**', 'test/**'])
